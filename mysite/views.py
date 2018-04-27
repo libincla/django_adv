0f1775f04c9cfd6ca42d44af7cf1e67930d93c04
libincla <554624371@qq.com>
2018-04-27 19:54:59 +0800

    add template and add base.html

--- mysite/views.py
+++ mysite/views.py
@@ -1,4 +1,7 @@
 from django.http import HttpResponse
+from django.template import Context
+from django.shortcuts import render
+from django.template.loader import get_template
 import datetime
 
 
@@ -6,11 +9,24 @@ import datetime
 def hi(request):
     return HttpResponse('hi wolrd!')
 
+#def current_datetime(request):
+#    now = datetime.datetime.now()
+#    now = str(now)[:-7]
+#    html = "it is %s now." % now
+#    return HttpResponse(html)
+
+#def current_datetime(request):
+#    now = datetime.datetime.now()
+#    now = str(now)[:-7]
+#    t = get_template('current_datetime.html')
+#    html = t.render(Context({'current_date': now }))
+#    return HttpResponse(html)
+
 def current_datetime(request):
     now = datetime.datetime.now()
     now = str(now)[:-7]
-    html = "it is %s now." % now
-    return HttpResponse(html)
+    return render(request, 'current_datetime.html', {'current_date' : now })
+
 
 
 def hours_ahead(request, offset):
@@ -21,4 +37,5 @@ def hours_ahead(request, offset):
     dt = datetime.datetime.now() + datetime.timedelta(hours=offset)
     dt = str(dt)[:-7]
     html = "in %s hour, it will be %s." % (offset, dt)
-    return HttpResponse(html)
+    context_dict = {'hour_offset' : offset, 'next_time' : dt }
+    return render(request, 'hours_ahead.html', context=context_dict)

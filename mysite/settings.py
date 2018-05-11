098fb21f127c8f2b2f7d549aea3b8b3dbc335461
libincla <554624371@qq.com>
2018-05-11 16:04:22 +0800

    chapter 16 and use database to save cache and latest chapter is too hard to learn

--- mysite/settings.py
+++ mysite/settings.py
@@ -86,6 +86,13 @@ DATABASES = {
 }
 
 
+CACHES = {
+    'default': {
+        'BACKEND' : 'django.core.cache.backends.db.DatabaseCache',
+        'LOCATION' : 'my_cache_table',
+    }
+}
+
 # Password validation
 # https://docs.djangoproject.com/en/1.9/ref/settings/#auth-password-validators
 
